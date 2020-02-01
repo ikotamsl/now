867d3aff79c43b2cdf8f4698a41ad0013a8f77e2
ivashka <ex@mail.com>
2020-02-01 22:15:36 +0300

    blya

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+// let arr = ['plum.png', 'me.png', 'life.png'];
+//
+// let ans = +prompt("Есть ли вам 18?", "Да");
+//
+// console.log(typeof(ans));
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+// let incr = 10,
+//     decr = 10;
+//
+// console.log(incr++, decr--); //Сначала выводит старое значение, а потом изменяет его
+//
+// console.log(5%2);
+// console.log("2" === 2);
+
+// let isChecked = false,
+//     isClosed = false;
+//
+// console.log(isChecked || !isClosed);
